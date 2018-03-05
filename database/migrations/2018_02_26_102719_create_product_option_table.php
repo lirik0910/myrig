689eb2571d62a0759f153e16090342a82e3d60f7
lirik0910 <webdevbastryhin@gmail.com>
2018-03-05 14:07:08 +0200

    zendesk

--- database/migrations/2018_02_26_102719_create_product_option_table.php
+++ database/migrations/2018_02_26_102719_create_product_option_table.php
@@ -6,23 +6,6 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProductOptionTable extends Migration
 {
-<<<<<<< HEAD
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('product_options', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('product_id')->unsigned();
-            $table->string('name', 255);
-            $table->text('value');
-            $table->timestamps();
-        });
-    }
-=======
 	/**
 	 * Run the migrations.
 	 *
@@ -34,7 +17,6 @@ class CreateProductOptionTable extends Migration
 			$table->engine = 'InnoDB';
 			$table->charset = 'utf8';
 			$table->collation = 'utf8_general_ci';
->>>>>>> 7d1c887b0cc4a0fd99475e033d90d28f7304e8b4
 
 			$table->increments('id');
 
--- database/migrations/2018_02_26_102741_add_relation_product_option.php
+++ database/migrations/2018_02_26_102741_add_relation_product_option.php
@@ -6,22 +6,6 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddRelationProductOption extends Migration
 {
-<<<<<<< HEAD
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('product_options', function (Blueprint $table){
-            $table->foreign('product_id')
-                ->references('id')
-                ->on('products')
-                ->onDelete('cascade');
-        });
-    }
-=======
 	/**
 	 * Run the migrations.
 	 *
@@ -36,7 +20,6 @@ class AddRelationProductOption extends Migration
 				->onDelete('cascade');
 		});
 	}
->>>>>>> 7d1c887b0cc4a0fd99475e033d90d28f7304e8b4
 
 	/**
 	 * Reverse the migrations.
