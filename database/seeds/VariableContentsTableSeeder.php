6f6fbefbcf6877fb846a93e2671c2fb2edcaa473
ihor <i.belchenko@makebecool.com>
2018-03-07 17:28:46 +0200

    update

--- database/seeds/VariableContentsTableSeeder.php
+++ database/seeds/VariableContentsTableSeeder.php
@@ -13,73 +13,73 @@ class VariableContentsTableSeeder extends Seeder
 	{
 		App\Model\Base\VariableContent::create([
 			'page_id' => 10,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => 'The quantity is limited!'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 10,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => 'Shipment from the factory in China April 20 - May 10.'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 10,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => '100% advance payment in BTC!'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 10,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => 'Final price check with the manager!'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 10,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => 'Warranty service: 1-5 days'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 10,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => 'Local delivery in Ukraine'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 11,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => 'The quantity is limited!'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 11,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => 'Shipment from the factory in China April 20 - May 10.'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 11,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => '100% advance payment in BTC!'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 11,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => 'Final price check with the manager!'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 11,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => 'Warranty service: 1-5 days'
 		]);
 
 		App\Model\Base\VariableContent::create([
 			'page_id' => 11,
-			'variable_id' => 5,
+			'variable_id' => 1,
 			'content' => 'Local delivery in Ukraine'
 		]);
 	}
--- resources/lang/en/shop.php
+++ resources/lang/en/shop.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-	'to_cart' => 'Add to cart'
+	'to_cart' => 'Add to cart',
+	'cont_USA' => 'USA',
+	'cont_RUSSIA' => 'RU',
+	'cont_UKRAINE' => 'UA',
+	'cont_JAPAN' => 'JAP',
+	'cont_VENEZUELA' => 'VE'
 ];
