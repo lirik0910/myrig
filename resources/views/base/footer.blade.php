32faafb8418a3c00a12fb0da3426f47cbf932060
lirik0910 <webdevbastryhin@gmail.com>
2018-04-12 16:58:35 +0300

    localization

--- resources/views/base/footer.blade.php
+++ resources/views/base/footer.blade.php
@@ -4,7 +4,7 @@ $context = $select('App\Model\Base\Context')->where('title', $locale)->first();
 
 $otherPages = $select('App\Model\Base\Page')
 	->where('parent_id', 0)
-	->where('context_id')
+	->where('context_id', $context->id)
 	->where(function ($q) {
 		return $q
 			->orWhere('view_id', 4)
