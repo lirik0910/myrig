c3b7e18dd6b76bcb8c4da7b31db0d520822041f1
lirik0910 <webdevbastryhin@gmail.com>
2018-04-18 21:05:29 +0300

    order fixes

--- resources/views/layouts/pdf.blade.php
+++ resources/views/layouts/pdf.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" href="{{ asset('css/pdf2.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/pdf.css') }}">
 	</head>
 	<body style="margin: 0; padding: 0; font: 400 18px Calibri, sans-serif;">
 @php
