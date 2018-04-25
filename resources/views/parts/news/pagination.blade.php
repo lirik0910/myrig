c4aaf0a167e967d3d7aa19d8ad39a3769d04f18b
lirik0910 <webdevbastryhin@gmail.com>
2018-04-25 10:34:30 +0300

    pagination

--- resources/views/parts/news/pagination.blade.php
+++ resources/views/parts/news/pagination.blade.php
@@ -1,6 +1,4 @@
 @php
-   // var_dump($paginator->url($paginator->currentPage()));
-    //var_dump($results->currentPage()); die;
     $current_page = $paginator->currentPage();
     $last_page = $paginator->lastPage();
 @endphp
@@ -10,25 +8,21 @@
         <li>
             <a class="prev page-numbers" href="{{ $paginator->previousPageUrl() }}" data-wpel-link="internal"><i class="article-arrow article-arrow-left"></i></a>
         </li>
-        @endif
-        @if($current_page > 3)
         <li>
-            <a class="page-numbers" href="{{ $paginator->firstItem() }}" data-wpel-link="internal">1</a>
-        </li>
-        <li>
-            <span class="page-numbers dots">…</span>
+            <a class="page-numbers" href="{{ $paginator->url(1) }}" data-wpel-link="internal">1</a>
         </li>
         @endif
         @if($current_page > 2)
         <li>
+            <span class="page-numbers dots">…</span>
+        </li>
+        <li>
             <a class="page-numbers" href="{{ $paginator->url($current_page - 1) }}" data-wpel-link="internal">{{ $current_page - 1 }}</a>
         </li>
         @endif
-        @if($current_page !== 1)
         <li>
             <span aria-current="page" class="page-numbers current">{{ $current_page }}</span>
         </li>
-        @endif
         @if(($current_page + 1) < $paginator->lastPage())
         <li>
             <a class="page-numbers" href="{{ $paginator->url($current_page + 1) }}" data-wpel-link="internal">{{ $current_page + 1 }}</a>
@@ -41,12 +35,11 @@
         @endif
         @if($current_page !== $last_page)
         <li>
-            <a class="page-numbers" href="{{ $paginator->url($paginator->lastPage()) }}" data-wpel-link="internal">{{ $paginator->lastPage() }}</a>
+            <a class="page-numbers" href="{{ $paginator->url($last_page) }}" data-wpel-link="internal">{{ $last_page }}</a>
         </li>
         <li>
             <a class="next page-numbers" href="{{ $paginator->nextPageUrl() }}" data-wpel-link="internal"><i class="article-arrow article-arrow-right"></i></a>
         </li>
         @endif
-
     </ul>
 </div>
