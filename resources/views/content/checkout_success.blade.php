64dd0f989b81e38bc13be1d362139012a3e8789c
ihor <ihor.bielchenko@ohmycode.studio>
2018-05-06 23:20:10 +0300

    update black container

--- resources/views/content/checkout_success.blade.php
+++ resources/views/content/checkout_success.blade.php
@@ -10,17 +10,6 @@
         @endphp
         <div class="main-back"  ></div>
         <script>
-            var width = $(window).width();
-            var cont = $('.container').outerWidth();
-            var margin = (width - cont) / 2;
-            var wM = cont * 33.333333 / 100 + margin;
-            if (width > 767) {
-                $('.main-back').css('left', wM +'px');
-            }
-            else {
-                $('.main-back').css('left', '0px');
-            }
-
             jQuery(document).ready(function($){
 
                 if ($("#timer").length) {
--- resources/views/content/service.blade.php
+++ resources/views/content/service.blade.php
@@ -3,18 +3,6 @@
 @section('content')
     <main>
         <div class="main-back"></div>
-        <script>
-            var width = $(window).width();
-            var cont = $('.container').outerWidth();
-            var margin = (width - cont) / 2;
-            var wM = cont * 33.333333 / 100 + margin;
-            if (width > 767) {
-                $('.main-back').css('left', wM +'px');
-            }
-            else {
-                $('.main-back').css('left', '0px');
-            }
-        </script>
         <section class="content single-article">
             <div class="container">
                 <div class="row">
