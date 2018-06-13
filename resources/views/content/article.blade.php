b1becfd1380d90717f41e03790ddfb91f4b2ceb0
Ivan Bastryhin <ivan.bastryhin@ohmycode.studio>
2018-06-13 17:09:23 +0300

    article page

--- resources/views/content/article.blade.php
+++ resources/views/content/article.blade.php
@@ -76,19 +76,19 @@ if (!$visits) {
 		<div class="d-block col-sm-4 margin__collapse padding__collapse"></div>
 		<div class="d-block col-sm-8 margin__collapse" style="border-top: 1px solid #FFF; padding: 32px;">
 			<div class="beside-items__container d-flex">
-				@isset($prev_link)
+				@if($prev_link)
 				<a href="{{ url($prev_link->link) }}" class="d-block default__link news-return__link font-weight-light" style="position: relative;">
 					<i class="article-arrow-left__icon"></i>
 					{{ __('default.previous_article') }}
 				</a>
-				@endisset
+				@endif
 
-				@isset($next_link)
+				@if($next_link)
 				<a href="{{ url($next_link->link) }}" class="d-block default__link news-return__link font-weight-light text-right" style="position: relative;">
 					<i class="article-arrow-right__icon" style="float: right"></i>
-					{{ __('next_article') }}
+					{{ __('default.next_article') }}
 				</a>
-				@endisset
+				@endif
 			</div>
 		</div>
 		@endif
