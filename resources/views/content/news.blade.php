980e08701f5727557e5f988c73fecec94a4249f8
Ivan Bastryhin <ivan.bastryhin@ohmycode.studio>
2018-06-20 14:04:28 +0300

    news page fix

--- resources/views/content/news.blade.php
+++ resources/views/content/news.blade.php
@@ -5,17 +5,17 @@
 @php
 $page_limit = 10;
 $page_no = $request->get('page');
-
+//var_dump($it->link); die;
 $ua_parent_page = \App\Model\Base\Page::where('link', $it->link)
-	->where('context_id', 2)
+	->where('context_id', 1)
 	->first();
 $ru_parent_page = \App\Model\Base\Page::where('link', $it->link)
-	->where('context_id', 3)
+	->where('context_id', 2)
 	->first();
-
+//var_dump($ru_parent_page); die;
 if (isset($page_no)) {
 	$news = App\Model\Base\Page::where('parent_id', $ua_parent_page->id)
-		->orWhere('parent_id', $ru_parent_page)
+		->orWhere('parent_id', $ru_parent_page->id)
 		->offset($page_limit * ($page_no - 1))
 		->orderBy('created_at', 'DESC')
 		->paginate($page_limit);
