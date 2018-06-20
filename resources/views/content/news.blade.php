a1c39ce8dbc3f0fecf60bbd6c47606bfb377902c
Ivan Bastryhin <ivan.bastryhin@ohmycode.studio>
2018-06-20 13:56:46 +0300

    news fix

--- resources/views/content/news.blade.php
+++ resources/views/content/news.blade.php
@@ -15,7 +15,7 @@ $ru_parent_page = \App\Model\Base\Page::where('link', $it->link)
 
 if (isset($page_no)) {
 	$news = App\Model\Base\Page::where('parent_id', $ua_parent_page->id)
-		->orWhere('parent_id', $ua_parent_page)
+		->orWhere('parent_id', $ru_parent_page)
 		->offset($page_limit * ($page_no - 1))
 		->orderBy('created_at', 'DESC')
 		->paginate($page_limit);
