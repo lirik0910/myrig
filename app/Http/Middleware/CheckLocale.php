51fa5f518d2a5a6678d8321fdfe3e0839a8b3106
ihor <ihor.bielchenko@ohmycode.studio>
2018-07-05 17:25:46 +0300

    test

--- app/Http/Middleware/CheckLocale.php
+++ app/Http/Middleware/CheckLocale.php
@@ -16,52 +16,73 @@ class CheckLocale
      */
     public function handle($request, Closure $next)
     {
-        if($request->method() == 'GET' || $request->method() == 'POST'){
-            $current_domain = $request->getSchemeAndHttpHost();
+        $method = $request->method();
+        if ($method === 'GET' || $method === 'POST') {
+            $current = $request->getSchemeAndHttpHost();
 
-            $clientIp = $_SERVER['REMOTE_ADDR'];
-            $locale = strtolower(geoip($clientIp)->iso_code);
-/*            if($locale !== 'ua' && $locale !== 'ru'){
-                $locale = 'en';
-            }*/
-            $sngCountries = [
-                'az', 'am', 'by', 'ge', 'kz', 'kg', 'ru', 'tm', 'uz'
-            ];
+            switch ($current) {
+                case config('app.ua_domain'):
+                    App::setLocale('ua');
+                    break;
 
-            if(in_array($locale, $sngCountries)){
-                $locale = 'ru';
-            } elseif ($locale !== 'ua' && !in_array($locale, $sngCountries)){
-                $locale = 'en';
+                case config('app.ru_domain'):
+                    App::setLocale('ru');
+                    break;
+
+                case config('app.en_domain'):
+                    App::setLocale('en');
+                    break;
             }
 
-            if($request->get('locale')){
-                session()->put('locale', $request->get('locale'));
-            }
-
-            if(session()->get('locale')){
-                $locale = session()->get('locale');
-            }
-
-            App::setLocale($locale);
-
-            $uaDomain = config('app.ua_domain');
-            $ruDomain = config('app.ru_domain');
-            $enDomain = config('app.en_domain');
-
-            if($locale == 'ua' && $current_domain !== $uaDomain || $locale == 'ru' && $current_domain !== $ruDomain || $locale =='en' && $current_domain !== $enDomain){
-                switch ($locale){
-                    case 'ua':
-                        return redirect(config('app.ua_domain'));
-                        break;
-                    case 'ru':
-                        return redirect(config('app.ru_domain'));
-                        break;
-                    case 'en':
-                        return redirect(config('app.en_domain'));
-                        break;
-                }
-            }
+            //geoip($_SERVER['REMOTE_ADDR'])->iso_code;
         }
+
+//         if($request->method() == 'GET' || $request->method() == 'POST'){
+//             $current_domain = $request->getSchemeAndHttpHost();
+
+//             $clientIp = $_SERVER['REMOTE_ADDR'];
+//             $locale = strtolower(geoip($clientIp)->iso_code);
+// /*            if($locale !== 'ua' && $locale !== 'ru'){
+//                 $locale = 'en';
+//             }*/
+//             $sngCountries = [
+//                 'az', 'am', 'by', 'ge', 'kz', 'kg', 'ru', 'tm', 'uz'
+//             ];
+
+//             if(in_array($locale, $sngCountries)){
+//                 $locale = 'ru';
+//             } elseif ($locale !== 'ua' && !in_array($locale, $sngCountries)){
+//                 $locale = 'en';
+//             }
+
+//             if($request->get('locale')){
+//                 session()->put('locale', $request->get('locale'));
+//             }
+
+//             if(session()->get('locale')){
+//                 $locale = session()->get('locale');
+//             }
+
+//             App::setLocale($locale);
+
+//             $uaDomain = config('app.ua_domain');
+//             $ruDomain = config('app.ru_domain');
+//             $enDomain = config('app.en_domain');
+
+//             if($locale == 'ua' && $current_domain !== $uaDomain || $locale == 'ru' && $current_domain !== $ruDomain || $locale =='en' && $current_domain !== $enDomain){
+//                 switch ($locale){
+//                     case 'ua':
+//                         return redirect(config('app.ua_domain'));
+//                         break;
+//                     case 'ru':
+//                         return redirect(config('app.ru_domain'));
+//                         break;
+//                     case 'en':
+//                         return redirect(config('app.en_domain'));
+//                         break;
+//                 }
+//             }
+//         }
 
         return $next($request);
     }
