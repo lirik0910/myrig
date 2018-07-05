586ad1b7030bf943867cd516b51e9cecc5cb4b29
ihor <ihor.bielchenko@ohmycode.studio>
2018-07-06 01:15:26 +0300

    test CheckLocale

--- app/Http/Middleware/CheckLocale.php
+++ app/Http/Middleware/CheckLocale.php
@@ -32,29 +32,37 @@ class CheckLocale
 			$currentHost = $request->getHttpHost();
 			$currentHostParts = explode('.', $currentHost);
 
-			$clientLocation = strtolower($this->defineCountry());
-			if (in_array($clientLocation, $this->sngCountries)) {
-				$clientLocation = 'ru';
-			}
-
-			else if ($clientLocation !== 'ua') {
-				$clientLocation = 'en';
-			}
-
-			$newLocale = $request->get('locale');
-			if ($newLocale) {
-				$_SESSION['locale'] = ($clientLocation = $newLocale);
-			}
-
-			else {
-				$clientLocation = $_SESSION['locale'] ?? ($_SESSION['locale'] = $clientLocation);
-			}
-
-			App::setLocale($clientLocation);
-			if ($currentHostParts[0] !== $clientLocation) {
-				return header('Location: ' . config('app.' . $clientLocation .'_domain'));
-			}
+			App::setLocale($currentHostParts[0]);
 		}
+
+		// $method = $request->method();
+		// if ($method === 'GET' || $method === 'POST') {
+		// 	$currentHost = $request->getHttpHost();
+		// 	$currentHostParts = explode('.', $currentHost);
+
+		// 	$clientLocation = strtolower($this->defineCountry());
+		// 	if (in_array($clientLocation, $this->sngCountries)) {
+		// 		$clientLocation = 'ru';
+		// 	}
+
+		// 	else if ($clientLocation !== 'ua') {
+		// 		$clientLocation = 'en';
+		// 	}
+
+		// 	$newLocale = $request->get('locale');
+		// 	if ($newLocale) {
+		// 		$_SESSION['locale'] = ($clientLocation = $newLocale);
+		// 	}
+
+		// 	else {
+		// 		$clientLocation = $_SESSION['locale'] ?? ($_SESSION['locale'] = $clientLocation);
+		// 	}
+
+		// 	App::setLocale($clientLocation);
+		// 	if ($currentHostParts[0] !== $clientLocation) {
+		// 		return header('Location: ' . config('app.' . $clientLocation .'_domain'));
+		// 	}
+		// }
 
 		return $next($request);
 	}
