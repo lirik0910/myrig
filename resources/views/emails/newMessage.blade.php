72df5c0e2360d91062152473c50abff967f9e784
ihor <ihor.bielchenko@ohmycode.studio>
2018-07-07 15:31:10 +0300

    test

--- resources/views/emails/newMessage.blade.php
+++ resources/views/emails/newMessage.blade.php
@@ -6,11 +6,6 @@
     <link rel="stylesheet" href="{{ asset('css/pdf.css') }}">
 </head>
 <body style="margin: 0; padding: 0; font: 400 18px Calibri, sans-serif;">
-@php
-    $order = App\Model\Shop\Order::where('number', $number)->with('orderDeliveries', 'products')->first();
-    $delivery = App\Model\Shop\Delivery::where('id', $order->orderDeliveries->delivery_id)->first();
-    $order_status = App\Model\Shop\OrderStatus::where('id', $order->status_id)->first();
-@endphp
 <div style="width: 500px; margin: 0 auto;">
     <p>{{__('default.email_message_title') }}</p>
     <p>{{ $text }}</p>
